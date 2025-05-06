dedf4201cd35636055735c617f29659d39d04f05
Christian <christian.vor26@gmail.com>
2025-05-06 21:01:48 +0200

    update

--- Arduino/PixleNet/PixleNet.ino
+++ Arduino/PixleNet/PixleNet.ino
@@ -1,11 +1,13 @@
 #include <FastLED.h>
 
-#define LED_PIN     25
-#define BUTTON_PIN  32
-#define NUM_LEDS    512
+#define LED_PIN_1     25
+#define LED_PIN_2     26
+#define BUTTON_PIN    32
+#define NUM_LEDS      512
 #define LONG_PRESS_DURATION 200  // in ms
 
-CRGB leds[NUM_LEDS];
+CRGB leds1[NUM_LEDS];
+CRGB leds2[NUM_LEDS];
 
 bool buttonPressed = false;
 bool lastButtonPressed = false;
@@ -14,25 +16,25 @@ unsigned long pressStartTime = 0;
 void setup() {
   Serial.begin(115200);
   pinMode(BUTTON_PIN, INPUT_PULLUP);  // Taster gegen GND
-  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
+
+  FastLED.addLeds<WS2812B, LED_PIN_1, GRB>(leds1, NUM_LEDS);
+  FastLED.addLeds<WS2812B, LED_PIN_2, GRB>(leds2, NUM_LEDS);
+  
   FastLED.setBrightness(50);
-  FastLED.clear();
-  FastLED.show();
+  FastLED.clear(true);
 }
 
 void loop() {
+  // --- Button Handling ---
   buttonPressed = digitalRead(BUTTON_PIN) == LOW;
 
   if (buttonPressed && !lastButtonPressed) {
-    // Taste wurde gedrückt
     pressStartTime = millis();
     fillAllRed();
   }
   else if (!buttonPressed && lastButtonPressed) {
-    // Taste wurde losgelassen
     unsigned long duration = millis() - pressStartTime;
-    FastLED.clear();
-    FastLED.show();
+    FastLED.clear(true);
 
     if (duration >= LONG_PRESS_DURATION) {
       Serial.println("a");  // langer Druck
@@ -42,11 +44,32 @@ void loop() {
   }
 
   lastButtonPressed = buttonPressed;
+
+  // --- Serial Input Handling ---
+  if (Serial.available() > 0) {
+    char input = Serial.read();
+    if (input == 'a') {
+      flashGreen(200);
+    } else if (input == 'b') {
+      flashGreen(50);
+    }
+  }
 }
 
 void fillAllRed() {
   for (int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = CRGB::Red;
+    leds1[i] = CRGB::Red;
+    leds2[i] = CRGB::Red;
   }
   FastLED.show();
+}
+
+void flashGreen(unsigned long duration) {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds1[i] = CRGB::Green;
+    leds2[i] = CRGB::Green;
+  }
+  FastLED.show();
+  delay(duration);
+  FastLED.clear(true);
 }
