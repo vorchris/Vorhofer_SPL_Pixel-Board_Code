857f59154437190404525c288efb653b3dfd5e16
Christian <christian.vor26@gmail.com>
2025-05-07 17:12:05 +0200

    Ok fix led test ok

--- Arduino/LedMatrix/LedMatrix.ino
+++ Arduino/LedMatrix/LedMatrix.ino
@@ -1,51 +1,59 @@
 #include <FastLED.h>
- 
-#define LED_PIN     25
-#define NUM_LEDS    512
-#define PANEL_WIDTH 32
-#define PANEL_HEIGHT 16
- 
-CRGB leds[NUM_LEDS];
- 
+
+#define NUM_LEDS 256
+#define WIDTH 32
+#define HEIGHT 16
+
+#define LEDS1_PIN 26
+#define LEDS2_PIN 25
+
+CRGB leds1[NUM_LEDS];
+CRGB leds2[NUM_LEDS];
+
 void setup() {
-    Serial.begin(115200);
-    FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
-    FastLED.setBrightness(50);
-}
- 
-int getPixel(int x, int y) {
-    int k = x / 2; 
-    int d = x % 2;   
-    
-    if (y < 8) {
-        int r = y;
-        if (d == 0)
-            return r + 16 * k;
-        else
-            return (15 - r) + 16 * k;
-    } else {    
-        int r = 15 - y; 
-        int bottom_val;
-        if (d == 0)
-            bottom_val = r + 16 * k;
-        else
-            bottom_val = (15 - r) + 16 * k;
-        return 511 - bottom_val;
-    }
+  FastLED.addLeds<WS2812B, LEDS1_PIN, GRB>(leds1, NUM_LEDS);
+  FastLED.addLeds<WS2812B, LEDS2_PIN, GRB>(leds2, NUM_LEDS);
+  FastLED.setBrightness(50);
+  FastLED.clear(true);
+  FastLED.show();
+
+  pixelTest();
 }
 
- 
 void loop() {
-    for (int y = 0; y < PANEL_HEIGHT; y++) {
-        for (int x = 0; x < PANEL_WIDTH; x++) {
-            int index = getPixel(x, y);
-            
-            leds[index] = CRGB::White;
-            FastLED.show();
-            delay(20);
+  // leer – der Test läuft nur einmal im Setup
+}
 
-            leds[index] = CRGB::Black;
-            FastLED.show();
-        }
+// Konvertiert x/y in LED-Index für serpentinierte Matrix
+int getLedIndex(int x, int y) {
+  int index = x * 8;
+  if (x % 2 == 0) {
+    index += 7 - y;
+  } else {
+    index += y;
+  }
+  return index;
+}
+
+// Setzt eine LED auf Farbe abhängig davon, ob sie zu Matrix 1 oder 2 gehört
+void setLed(int x, int y, CRGB color) {
+  if (y < 8) {
+    leds1[getLedIndex(31 - x, 7 - y)] = color;  // untere Hälfte (y: 0–7)
+  } else {
+    leds2[getLedIndex(x, y - 8)] = color;       // obere Hälfte (y: 8–15)
+  }
+  FastLED.show();
+}
+
+// Testet jede LED nacheinander
+void pixelTest() {
+  FastLED.clear(true);
+  for (int y = 0; y < HEIGHT; y++) {
+    for (int x = 0; x < WIDTH; x++) {
+      setLed(x, y, CRGB::White);
+      delay(30);
+      setLed(x, y, CRGB::Black);
     }
+  }
+  FastLED.show();
 }
